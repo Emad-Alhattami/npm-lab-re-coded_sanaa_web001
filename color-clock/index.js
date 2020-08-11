49fcc9fbe47eb41a5bdbb634ccf5744bccd9f17b
Emad Alhattami <emad774373827@gmail.com>
2020-08-11 16:34:28 +0000

    Automatically backed up by Learn

--- color-clock/index.js
+++ color-clock/index.js
@@ -1,16 +1,17 @@
-//No need to do any work here.  
-//If package.json and the proper modules are installed in the correct location, this should display correctly in the browser.
-
-function displayTime() {
-  let clock = document.querySelector('#clock')
-  clock.innerText = moment().format('MMMM Do YYYY, h:mm:ss a')
-  setInterval(function() {
-      clock.innerText = moment().format('MMMM Do YYYY, h:mm:ss a')
-      clock.style.color = '#'+Math.floor(Math.random()*16777215).toString(16);
-      clock.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-  }, 1000)
-
-
+{
+  "name": "emad",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo 'Hello World!'"
+  },
+  "keywords": [
+    "emad"
+  ],
+  "author": "emad",
+  "license": "ISC",
+  "dependencies": {
+    "moment": "^2.27.0"
+  }
 }
-
-window.onload = displayTime()
